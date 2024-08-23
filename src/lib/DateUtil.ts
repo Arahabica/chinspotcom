2a71af136f7745e039c08b0e077f44f263d0cc10
Arahabica <r.sasage@gmail.com>
2024-08-24 00:25:57 +0900

    timezoneのバグを修正

--- src/lib/DateUtil.ts
+++ src/lib/DateUtil.ts
@@ -1,5 +1,10 @@
 export const getMidnight = (date: Date): Date => {
-  const nextDay = new Date(date);
+  const nextDay = japanDate(date);
   nextDay.setHours(0, 0, 0, 0);
   return nextDay;
+}
+
+export const japanDate = (date: Date): Date => {
+  const offset = date.getTimezoneOffset() - 9 * 60;
+  return new Date(date.getTime() + offset * 60 * 1000);
 }
