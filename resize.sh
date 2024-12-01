929f6d65332ac78574c35a3dca956aca010975c9
Arahabica <r.sasage@gmail.com>
2024-12-02 01:03:02 +0900

    リサイズ機能の修正

--- resize.sh
+++ resize.sh
@@ -6,7 +6,7 @@ fi
 target_dir="$1"
 
 
-for file in public/images/${target_dir}/*.{jpg,png}; do
+for file in public/images/${target_dir}/*.{jpg,png,webp}; do
     # Get image width using identify command
     width=$(identify -format "%w" "$file")
     
@@ -14,8 +14,13 @@ for file in public/images/${target_dir}/*.{jpg,png}; do
     if [ $width -gt 800 ]; then
         filename=$(basename "$file")
         basename="${filename%.*}"
-        convert "$file" -resize 800x -quality 94 "$(dirname "$file")/${basename}_800x.webp"
+        converted_filename="$(dirname "$file")/${basename}_800x.webp"
+        convert "$file" -resize 800x -quality 94 "$converted_filename"
         # 元のファイルを削除
         rm "$file"
+        # 変更後の画像の高さを取得
+        height=$(identify -format "%h" "$converted_filename")
+        new_filename="$(dirname "$file")/${basename}_800x${height}.webp"
+        mv "$converted_filename" "$new_filename"
     fi
 done
